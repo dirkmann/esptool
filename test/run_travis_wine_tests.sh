4a9a228ebabeee141c929e4c72316e75e893eca0
Angus Gratton <angus@espressif.com>
2017-04-21 18:22:47 +1000

    Travis Fix typo

--- test/run_travis_wine_tests.sh
+++ test/run_travis_wine_tests.sh
@@ -8,7 +8,7 @@ set
 echo "****"
 printenv WINDOWS_PYTHON_${TRAVIS_PYTHON_VERSION//./}_DL
 
-PYTHON_DL_URL=$(printenv WINDOWS_PYTHON_${TRAVIS_PYTHON_VERSION//./}_DL)
+PYTHON_DL_URL=$(printenv WINDOWS_PYTHON_${TRAVIS_PYTHON_VERSION//./}_DL
 
 [ -z $PYTHON_DL_URL ] && echo "Expected PYTHON_DL_URL to be set." && exit 1
 
@@ -25,7 +25,7 @@ PYTHON_INSTALL=$(basename $PYTHON_DL_URL)
 # Windows Python installers are either MSI or WiX-based
 # executable installers...
 if [[ $(basename $PYTHON_DL_URL) == *.msi ]]; then
-	wine msiexec /i $(PYTHON_INSTALL)
+	wine msiexec /i ${PYTHON_INSTALL}
 else
 	xvfb-run wine ${PYTHON_INSTALL} /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 fi
