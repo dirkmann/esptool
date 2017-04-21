38adcf3d3bb93bb69758960507374d999eb0891a
Angus Gratton <angus@espressif.com>
2017-04-21 17:43:19 +1000

    Travis: Experiment with running Windows tests in wine
    Requires "sudo: required" to be set in Travis :(

---
+++ test/run_travis_wine_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Run some basic sanity tests using wine
+# Designed to be run under Travis
+#
+
+[ -n $PYTHON_DL_URL ] || ( echo "Expected PYTHON_DL_URL to be set." && exit 1 )
+
+mkdir wineroot
+export WINEPREFIX="`pwd`/wineroot"
+export WINEARCH=win32
+unset DISPLAY
+
+set -ex
+
+wget --continue $PYTHON_DL_URL
+PYTHON_INSTALL=$(basename $PYTHON_DL_URL)
+
+# Windows Python installers are either MSI or WiX-based
+# executable installers...
+if [[ $(basename $PYTHON_DL_URL) == *.msi ]]; then
+	wine msiexec /i $(PYTHON_INSTALL)
+else
+	xvfb-run wine ${PYTHON_INSTALL} /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+fi
+
+wine pip install .
+wine esptool.py --help
+wine espsecure.py --help
+wine espefuse.py --help
+
+echo "esptool.py installed OK under Windows..."
+
